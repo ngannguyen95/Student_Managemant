9762710a2329f36389db5b94eee4d7c4724ec3db
Nguyencongnguyen1812 <ngannt33@wru.vn>
2023-02-22 16:50:26 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import Control from './components/Control';
+import ListStudents from './components/ListStudents';
+import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="row">
+        <div className="col-lg-7 grid-margin stretch-card">
+          <div className="card">
+            {/* START CONTROL */}
+            <Control />
+            {/* END CONTROL */}
+            {/* START LIST STUDENT */}
+            <ListStudents />
+            {/* END LIST STUDENT */}
+          </div>
+        </div>
+        {/* START FORM SINH VIEN */}
+        <Form />
+        {/* END FORM SINH VIÊN */}
+      </div>
+
     </div>
   );
 }
